d3c40e692161457f122ca2fd7e017d8415fde676
jose Jerez <josejerez2296@gmail.com>
2017-10-22 02:57:03 +0200

    Add Ajustes and tops
    Refactor css

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('inicio', { pagina: 'Inicio' });
+    res.render('inicio', { titulo: 'Inicio', pagina: 'Inicio' });
 });
 
 router.get('/Inicio', function(req, res, next) {
-    res.render('inicio', { pagina: 'Inicio' });
+    res.render('inicio', { titulo: 'Inicio', pagina: 'Inicio' });
 });
 
 router.get('/Perfil', function(req, res, next) {
     res.render('perfil',
         {
+            titulo: 'Perfil',
             pagina: 'Perfil',
             id: 0 ,
             top: ["Cancion 1","Cancion 2","Cancion 3","Cancion 4","Cancion 5"],
@@ -21,11 +22,41 @@ router.get('/Perfil', function(req, res, next) {
     );
 });
 
+router.get('/Perfil/Ajustes', function(req, res, next) {
+    res.render('Ajustes',
+        {
+            titulo: 'Ajustes',
+            pagina: 'Ajustes',
+            id: 0
+        }
+    );
+});
+
+router.get('/Perfil/Top-Canciones', function(req, res, next) {
+    res.render('Top-Canciones',
+        {
+            titulo: 'Top-Canciones',
+            pagina: 'Top Canciones',
+            id: 0
+        }
+    );
+});
+
+router.get('/Perfil/PlayList', function(req, res, next) {
+    res.render('PlayList',
+        {
+            titulo: 'PlayList',
+            pagina: 'PlayList',
+            id: 0
+        }
+    );
+});
+
 router.get('/Generos', function(req, res, next) {
-    res.render('generos', { pagina: 'Generos' });
+    res.render('generos', { titulo: 'Generos', pagina: 'Generos' });
 });
 
 router.get('/Chats', function(req, res, next) {
-    res.render('chats', { pagina: 'Chats' });
+    res.render('chats', { titulo: 'Chats', pagina: 'Chats' });
 });
 module.exports = router;
